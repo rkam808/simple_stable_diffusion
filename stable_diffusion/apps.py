15ae17fd457dcb83eed730251fc36ac39a08ed72
Ryan Kam <rkam0725@gmail.com>
2023-09-26 15:45:38 +0900

    initial commit

---
+++ stable_diffusion/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class StableDiffusionConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'stable_diffusion'
---
+++ stable_diffusion/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+import requests
+
+def home(request):
+    return render(request, 'stable_diffusion/home.html')
