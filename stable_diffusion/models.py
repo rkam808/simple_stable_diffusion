c1184f7c69413386307b80b4d5142d37e3d148e0
Ryan Kam <rkam0725@gmail.com>
2023-10-02 22:34:44 +0900

    migrations

--- stable_diffusion/models.py
+++ stable_diffusion/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Prompt(models.Model):
+    description = models.CharField(default='', max_length=1000)
+    type = models.CharField(default='', max_length=100)
+    
+    def __str__(self):
+        return self.description
+
+class Example(models.Model):
+    name = models.CharField(default='', max_length=100)
+    image_path = models.CharField(default='', max_length=100)
+    prompts = models.ManyToManyField(Prompt)
+    
+    def __str__(self):
+        return self.name
