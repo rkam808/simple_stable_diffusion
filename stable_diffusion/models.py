ab835f5a554de033c5c5197ad3c81d1cf0accc6e
Ryan Kam <rkam0725@gmail.com>
2023-10-06 17:15:09 +0900

    update models

--- stable_diffusion/models.py
+++ stable_diffusion/models.py
@@ -11,6 +11,9 @@ class Example(models.Model):
     name = models.CharField(default='', max_length=100)
     image_path = models.CharField(default='', max_length=100)
     prompts = models.ManyToManyField(Prompt)
+    style_prompt = models.CharField(default='', max_length=1000)
+    character_prompt = models.CharField(default='', max_length=1000)
+    negative_prompt = models.CharField(default='', max_length=1000)
     
     def __str__(self):
         return self.name
