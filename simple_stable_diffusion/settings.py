3518f28d7994774028ef5b325b1b03aa536c4ff8
Ryan Kam <rkam0725@gmail.com>
2023-10-06 17:14:39 +0900

    setup static images

--- simple_stable_diffusion/settings.py
+++ simple_stable_diffusion/settings.py
@@ -124,6 +124,19 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
+}
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
